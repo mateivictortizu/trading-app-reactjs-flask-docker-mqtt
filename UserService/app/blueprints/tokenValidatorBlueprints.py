e7b8c19195103e183929892be3b1f16084459d62
Matei <matteovk@icloud.com>
2022-02-21 00:44:25 +0200

    Check token blueprint

---
+++ UserService/app/blueprints/tokenValidatorBlueprints.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, jsonify
+from flask_json_schema import JsonValidationError
+from sqlalchemy.exc import DatabaseError
+
+tokenValidatorBP = Blueprint('tokenValidatorBlueprint', __name__)
+
+
+@tokenValidatorBP.errorhandler(JsonValidationError)
+def validator_error(e):
+    return jsonify({'error': e.message, 'errors': [validation_error.message for validation_error in e.errors]}), 400
+
+
+@tokenValidatorBP.errorhandler(DatabaseError)
+def database_error():
+    return jsonify({'error': 'Database error'}), 500
+
+
+@tokenValidatorBP.route('/check-token', methods=['GET'])
+def check_token():
+    pass
