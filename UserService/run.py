9b3fae3af60cdfec005475515f664006ce00633b
Matei <matteovk@icloud.com>
2022-03-08 01:36:50 +0200

    Docker rework

--- UserService/run.py
+++ UserService/run.py
@@ -3,8 +3,9 @@ import os
 import time
 
 if __name__ == "__main__":
-    #os.system('flask db stamp head')
-    #os.system('flask db migrate')
-    #os.system('flask db upgrade')
-    #time.sleep(3)
-    app.run(host='127.0.0.1', port=5001, debug=True, ssl_context=('cert.pem', 'key.pem'))
+    os.system('flask db init')
+    os.system('flask db stamp head')
+    os.system('flask db migrate')
+    os.system('flask db upgrade')
+    time.sleep(3)
+    app.run(host='0.0.0.0', port=5001, debug=True, ssl_context=('cert.pem', 'key.pem'))
