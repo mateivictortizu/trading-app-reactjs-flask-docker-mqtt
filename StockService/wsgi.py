76b939cba2316f7f933981c5ad20f2155add0428
Matei <matteovk@icloud.com>
2022-03-11 12:48:10 +0200

    Nginx and docker

--- StockService/wsgi.py
+++ StockService/wsgi.py
@@ -4,8 +4,9 @@ import time
 from app import app
 
 if __name__ == "__main__":
+    os.system('flask db init')
     os.system('flask db stamp head')
     os.system('flask db migrate')
     os.system('flask db upgrade')
     time.sleep(3)
-    app.run(host='0.0.0.0', debug=False, ssl_context=('cert.pem', 'key.pem'))
+    app.run()
--- UserService/wsgi.py
+++ UserService/wsgi.py
@@ -1,11 +1,4 @@
 from app import application
-import os
-import time
 
 if __name__ == "__main__":
-    os.system('flask db init')
-    os.system('flask db stamp head')
-    os.system('flask db migrate')
-    os.system('flask db upgrade')
-    time.sleep(3)
     application.run()
