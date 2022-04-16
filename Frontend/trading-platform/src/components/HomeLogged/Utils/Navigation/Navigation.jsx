37417623307d40b198809fced3d72fdec2e2cd3a
Tizu Matei Victor <matteovk@icloud.com>
2022-04-16 16:44:12 +0300

    Add sidebar

--- Frontend/trading-platform/src/components/HomeLogged/Utils/Navigation/Navigation.jsx
+++ Frontend/trading-platform/src/components/HomeLogged/Utils/Navigation/Navigation.jsx
@@ -1,12 +1,71 @@
 import "./Navigation.css";
 import React from "react";
+import { Button } from "@mui/material";
+import HomeIcon from '@mui/icons-material/Home';
+import PieChartIcon from '@mui/icons-material/PieChart';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
-export default function Navigation() {
+export default function Navigation({buttonClicked,setButtonClicked}) {
 
+    function clickHome() {
+        setButtonClicked('home');
+    }
+
+    function clickPie() {
+        setButtonClicked('pie');
+    }
+
+    function clickSearch() {
+        setButtonClicked('search');
+    }
+
+    function clickNotification() {
+        setButtonClicked('notification');
+    }
 
     return (
-        <div>
-            Da
+        <div class="sidebar">
+            {(buttonClicked==='home') ? (
+                <Button title='Home' id='buttonClickedSidebarLogged'>
+                    <HomeIcon />
+                </Button>
+            ) : (
+                <Button title='Home' id='buttonSidebarLogged' onClick={clickHome}>
+                    <HomeIcon />
+                </Button>
+            )}
+
+            {(buttonClicked==='pie') ? (
+                <Button title='Portofolio' id='buttonClickedSidebarLogged'>
+                    <PieChartIcon />
+                </Button>
+            ) : (
+                <Button title='Portofolio' id='buttonSidebarLogged' onClick={clickPie}>
+                    <PieChartIcon />
+                </Button>
+            )}
+
+            {(buttonClicked==='search') ? (
+                <Button title='Search' id='buttonClickedSidebarLogged'>
+                    <SearchIcon />
+                </Button>
+            ) : (
+                <Button title='Search' id='buttonSidebarLogged' onClick={clickSearch}>
+                    <SearchIcon />
+                </Button>
+            )}
+
+            {(buttonClicked==='notification') ? (
+                <Button title='Notification' id='buttonClickedSidebarLogged'>
+                    <NotificationsIcon />
+                </Button>
+            ) : (
+                <Button title='Notification' id='buttonSidebarLogged' onClick={clickNotification}>
+                    <NotificationsIcon />
+                </Button>
+            )}
+
         </div>
     );
 }
--- Frontend/trading-platform/src/components/HomeLogged/index.jsx
+++ Frontend/trading-platform/src/components/HomeLogged/index.jsx
@@ -2,18 +2,33 @@ import React from 'react';
 import Header from "./Utils/Header/Header"
 import './HomeLogged.css';
 import Navigation from './Utils/Navigation/Navigation';
+import NavigationHome from './Utils/NavigationHome/NavigationHome';
+import { Grid } from '@mui/material';
 
 
 const HomeLogged = () => {
+
     document.title = 'Your Dashbord'
+    const [buttonClicked, setButtonClicked] = React.useState('home');
+
     return (
         <div className='mainDivLogged'>
             <Header />
             <div className="firstDivLogged">
-                <Navigation />
-            </div>
-            <div id='footer'>
-                footer
+                <Grid container spacing={1}>
+                    <Grid item >
+                        <Navigation
+                            buttonClicked={buttonClicked}
+                            setButtonClicked={setButtonClicked}
+                        />
+                    </Grid>
+
+                    {(buttonClicked=='home') &&
+                        <Grid item>
+                            <NavigationHome />
+                        </Grid>
+                    }
+                </Grid>
             </div>
         </div>
     );
