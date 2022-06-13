0dc84e99c4d4033f19eb370dfc35d3a9f22fe1cf
Tizu Matei Victor <matteovk@icloud.com>
2022-06-14 01:22:07 +0300

    UserReaper 1/2

--- Gateway/app/blueprints/userService.py
+++ Gateway/app/blueprints/userService.py
@@ -1,115 +1,264 @@
-from urllib import parse
+import json
 
-from flask import Blueprint, request
-import requests
+from flask import Blueprint, request, jsonify
+
+from app.RabbitMQClients.UserRabbitMQ import ResendOTPClient, BanClient, VerifyUserClient, CheckTokenClient, \
+    RegisterClient, LoginClient, ResendValidateAccountClient, ValidateOTPClient, LogoutClient, ChangePasswordClient, \
+    RequestChangePasswordClient, ResetPassClient, SetNewPassClient
 
 user = Blueprint('user', __name__)
 
 URL = "http://127.0.0.1:5003/"
 
+ban_client = None
+verify_user_client = None
+check_token_client = None
+register_client = None
+login_client = None
+validate_account_client = None
+resend_validate_account_client = None
+validate_otp_client = None
+resend_otp_client = None
+logout_client = None
+change_password_client = None
+request_change_password_client = None
+reset_pass_client = None
+set_new_pass_client = None
+
 
 @user.route('/ban', methods=['DELETE'])
 def ban_user():
-    json_body = request.json
-    headers = request.headers
-    r = requests.delete(parse.urljoin(URL, "ban"), json=json_body, headers=headers)
-    return r.content, r.status_code
+    global ban_client
+    if ban_client is None:
+        ban_client = BanClient()
+    try:
+        response = json.loads(ban_client.call(request.json))
+        return response, response['code']
+    except Exception:
+        try:
+            ban_client = BanClient()
+            response = json.loads(ban_client.call(request.json))
+            return response, response['code']
+        except Exception:
+            return jsonify({'error': 'Ban server error', 'code': 500}), 500
 
 
 @user.route('/verify-user', methods=['PUT'])
 def verify_user():
-    json_body = request.json
-    headers = request.headers
-    r = requests.put(parse.urljoin(URL, "verify-user"), json=json_body, headers=headers)
-    return r.content, r.status_code
+    global verify_user_client
+    if verify_user_client is None:
+        verify_user_client = VerifyUserClient()
+    try:
+        response = json.loads(verify_user_client.call(request.json))
+        return response, response['code']
+    except Exception:
+        try:
+            verify_user_client = VerifyUserClient()
+            response = json.loads(verify_user_client.call(request.json))
+            return response, response['code']
+        except Exception:
+            return jsonify({'error': 'Verify user server error', 'code': 500}), 500
 
 
 @user.route('/check-token', methods=['GET'])
 def check_token():
-    pass
+    global check_token_client
+    if check_token_client is None:
+        check_token_client = CheckTokenClient()
+    try:
+        response = json.loads(check_token_client.call(request.json))
+        return response, response['code']
+    except Exception:
+        try:
+            check_token_client = CheckTokenClient()
+            response = json.loads(check_token_client.call(request.json))
+            return response, response['code']
+        except Exception:
+            return jsonify({'error': 'Check token server error', 'code': 500}), 500
 
 
 @user.route('/register', methods=['POST'])
 def register():
-    json_body = request.json
-    headers = request.headers
-    r = requests.post(parse.urljoin(URL, "register"), json=json_body, headers=headers)
-    return r.content, r.status_code
+    global register_client
+    if register_client is None:
+        register_client = RegisterClient()
+    try:
+        response = json.loads(register_client.call(request.json))
+        return response, response['code']
+    except Exception:
+        try:
+            register_client = RegisterClient()
+            response = json.loads(register_client.call(request.json))
+            return response, response['code']
+        except Exception:
+            return jsonify({'error': 'Register server error', 'code': 500}), 500
 
 
 @user.route('/login', methods=['POST'])
 def login():
-    json_body = request.json
-    headers = request.headers
-    r = requests.post(parse.urljoin(URL, "login"), json=json_body, headers=headers)
-    return r.content, r.status_code
+    global login_client
+    if login_client is None:
+        login_client = LoginClient()
+    try:
+        response = json.loads(login_client.call(request.json))
+        return response, response['code']
+    except Exception:
+        try:
+            login_client = LoginClient()
+            response = json.loads(login_client.call(request.json))
+            return response, response['code']
+        except Exception:
+            return jsonify({'error': 'Login server error', 'code': 500}), 500
 
 
 @user.route('/validate-account/<validation_code>', methods=['GET'])
 def validate_account(validation_code):
-    headers = request.headers
-    r = requests.get(parse.urljoin(URL, "validate-account/"+validation_code), headers=headers)
-    return r.content, r.status_code
+    global validate_account_client
+    if validate_account_client is None:
+        validate_account_client = LoginClient()
+    try:
+        response = json.loads(validate_account_client.call({'validation_code': validation_code}))
+        return response, response['code']
+    except Exception:
+        try:
+            validate_account_client = LoginClient()
+            response = json.loads(validate_account_client.call({'validation_code': validation_code}))
+            return response, response['code']
+        except Exception:
+            return jsonify({'error': 'Validate account server error', 'code': 500}), 500
 
 
 @user.route('/resend-validate-account', methods=['POST'])
 def resend_validate_account():
-    json_body = request.json
-    headers = request.headers
-    r = requests.post(parse.urljoin(URL, "resend-validate-account"), json=json_body, headers=headers)
-    return r.content, r.status_code
+    global resend_validate_account_client
+    if resend_validate_account_client is None:
+        resend_validate_account_client = ResendValidateAccountClient()
+    try:
+        response = json.loads(resend_validate_account_client.call(request.json))
+        return response, response['code']
+    except Exception:
+        try:
+            resend_validate_account_client = ResendValidateAccountClient()
+            response = json.loads(resend_validate_account_client.call(request.json))
+            return response, response['code']
+        except Exception:
+            return jsonify({'error': 'Resend validate account server error', 'code': 500}), 500
 
 
 @user.route('/validate-otp', methods=['POST'])
 def validate_otp():
-    json_body = request.json
-    headers = request.headers
-    r = requests.post(parse.urljoin(URL, "validate-otp"), json=json_body, headers=headers)
-    return r.content, r.status_code
+    global validate_otp_client
+    if validate_otp_client is None:
+        validate_otp_client = ValidateOTPClient()
+    try:
+        response = json.loads(validate_otp_client.call(request.json))
+        return response, response['code']
+    except Exception:
+        try:
+            validate_otp_client = ValidateOTPClient()
+            response = json.loads(validate_otp_client.call(request.json))
+            return response, response['code']
+        except Exception:
+            return jsonify({'error': 'Validate OTP account server error', 'code': 500}), 500
 
 
 @user.route('/resend-otp', methods=['POST'])
 def resend_otp():
-    json_body = request.json
-    headers = request.headers
-    r = requests.post(parse.urljoin(URL, "resend-otp"), json=json_body, headers=headers)
-    return r.content, r.status_code
+    global resend_otp_client
+    if resend_otp_client is None:
+        resend_otp_client = ResendOTPClient()
+    try:
+        response = json.loads(resend_otp_client.call(request.json))
+        return response, response['code']
+    except Exception:
+        try:
+            resend_otp_client = ResendOTPClient()
+            response = json.loads(resend_otp_client.call(request.json))
+            return response, response['code']
+        except Exception:
+            return jsonify({'error': 'Resend OTP server error', 'code': 500}), 500
 
 
 @user.route('/logout', methods=['DELETE'])
 def logout():
-    json_body = request.json
-    headers = request.headers
-    r = requests.delete(parse.urljoin(URL, "logout"), json=json_body, headers=headers)
-    return r.content, r.status_code
+    global logout_client
+    if logout_client is None:
+        logout_client = LogoutClient()
+    try:
+        response = json.loads(logout_client.call(request.json))
+        return response, response['code']
+    except Exception:
+        try:
+            logout_client = LogoutClient()
+            response = json.loads(logout_client.call(request.json))
+            return response, response['code']
+        except Exception:
+            return jsonify({'error': 'Logout server error', 'code': 500}), 500
 
 
 @user.route('/change-password', methods=['PUT'])
 def change_password():
-    json_body = request.json
-    headers = request.headers
-    r = requests.put(parse.urljoin(URL, "change-password"), json=json_body, headers=headers)
-    return r.content, r.status_code
+    global change_password_client
+    if change_password_client is None:
+        change_password_client = ChangePasswordClient()
+    try:
+        response = json.loads(change_password_client.call(request.json))
+        return response, response['code']
+    except Exception:
+        try:
+            change_password_client = ChangePasswordClient()
+            response = json.loads(change_password_client.call(request.json))
+            return response, response['code']
+        except Exception:
+            return jsonify({'error': 'Change Password server error', 'code': 500}), 500
 
 
 @user.route('/request-change-password', methods=['POST'])
 def request_change_password():
-    json_body = request.json
-    headers = request.headers
-    r = requests.post(parse.urljoin(URL, "request-change-password"), json=json_body, headers=headers)
-    return r.content, r.status_code
+    global request_change_password_client
+    if request_change_password_client is None:
+        request_change_password_client = RequestChangePasswordClient()
+    try:
+        response = json.loads(request_change_password_client.call(request.json))
+        return response, response['code']
+    except Exception:
+        try:
+            request_change_password_client = RequestChangePasswordClient()
+            response = json.loads(request_change_password_client.call(request.json))
+            return response, response['code']
+        except Exception:
+            return jsonify({'error': 'Request Change Password server error', 'code': 500}), 500
 
 
 @user.route('/reset-pass/<reset_code>', methods=['GET'])
 def reset_pass(reset_code):
-    headers = request.headers
-    r = requests.get(parse.urljoin(URL, "reset-pass/" + reset_code), headers=headers)
-    return r.content, r.status_code
+    global reset_pass_client
+    if reset_pass_client is None:
+        reset_pass_client = ResetPassClient()
+    try:
+        response = json.loads(reset_pass_client.call({"reset_code": reset_code}))
+        return response, response['code']
+    except Exception:
+        try:
+            reset_pass_client = ResetPassClient()
+            response = json.loads(reset_pass_client.call({"reset_code": reset_code}))
+            return response, response['code']
+        except Exception:
+            return jsonify({'error': 'Reset Pass server error', 'code': 500}), 500
 
 
 @user.route('/set-new-pass', methods=['POST'])
 def set_new_pass():
-    json_body = request.json
-    headers = request.headers
-    r = requests.post(parse.urljoin(URL, "set-new-pass"), json=json_body, headers=headers)
-    return r.content, r.status_code
+    global set_new_pass_client
+    if set_new_pass_client is None:
+        set_new_pass_client = SetNewPassClient()
+    try:
+        response = json.loads(set_new_pass_client.call(request.json))
+        return response, response['code']
+    except Exception:
+        try:
+            set_new_pass_client = SetNewPassClient()
+            response = json.loads(set_new_pass_client.call(request.json))
+            return response, response['code']
+        except Exception:
+            return jsonify({'error': 'Set New Pass server error', 'code': 500}), 500
--- Reapers/UserReaper/wsgi.py
+++ Reapers/UserReaper/wsgi.py
@@ -1,16 +1,103 @@
-# This is a sample Python script.
+import json
+from urllib import parse
+import requests
+import pika
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+URL = "http://127.0.0.1:5003/"
 
 
-# Press the green button in the gutter to run the script.
+def on_ban(ch, method, props, body):
+    pass
+
+
+def on_verify_user(ch, method, props, body):
+    pass
+
+
+def on_check_token(ch, method, props, body):
+    pass
+
+
+def on_register(ch, method, props, body):
+    pass
+
+
+def on_login(ch, method, props, body):
+    pass
+
+
+def on_validate_account(ch, method, props, body):
+    pass
+
+
+def on_resend_validate_account(ch, method, props, body):
+    pass
+
+
+def on_validate_otp(ch, method, props, body):
+    pass
+
+
+def on_resend_otp(ch, method, props, body):
+    pass
+
+
+def on_logout(ch, method, props, body):
+    pass
+
+
+def on_change_password(ch, method, props, body):
+    pass
+
+
+def on_request_change_password(ch, method, props, body):
+    pass
+
+
+def on_reset_pass(ch, method, props, body):
+    pass
+
+
+def on_set_new_pass(ch, method, props, body):
+    pass
+
+
+def start():
+    print('Stock RabbitMQ Server start...')
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost'))
+
+    channel = connection.channel()
+
+    channel.queue_declare(queue='add-stock')
+    channel.queue_declare(queue='get-stock-info')
+    channel.queue_declare(queue='get-stock-price')
+    channel.queue_declare(queue='get-list-stock-price')
+    channel.queue_declare(queue='update-price')
+    channel.queue_declare(queue='update-stock')
+    channel.queue_declare(queue='get-all-stocks')
+
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(queue='ban', on_message_callback=on_ban)
+    channel.basic_consume(queue='verify-user', on_message_callback=on_verify_user)
+    channel.basic_consume(queue='check-token', on_message_callback=on_check_token)
+    channel.basic_consume(queue='register', on_message_callback=on_register)
+    channel.basic_consume(queue='login', on_message_callback=on_login)
+    channel.basic_consume(queue='validate-account', on_message_callback=on_validate_account)
+    channel.basic_consume(queue='resend-validate-account', on_message_callback=on_resend_validate_account)
+    channel.basic_consume(queue='validate-otp', on_message_callback=on_validate_otp)
+    channel.basic_consume(queue='resend-otp', on_message_callback=on_resend_otp)
+    channel.basic_consume(queue='logout', on_message_callback=on_logout)
+    channel.basic_consume(queue='change-password', on_message_callback=on_change_password)
+    channel.basic_consume(queue='request-change-password', on_message_callback=on_request_change_password)
+    channel.basic_consume(queue='reset-pass', on_message_callback=on_reset_pass)
+    channel.basic_consume(queue='set-new-pass', on_message_callback=on_set_new_pass)
+
+    try:
+        channel.start_consuming()
+    except KeyboardInterrupt:
+        print('Stock RabbitMQ Server down.')
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    start()
